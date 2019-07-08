efa924b91bad3c9afc3f4374a7efa01a3e1f9b4b
Nikhil Balyan <nbalyan10@gmail.com>
2019-07-08 17:43:40 +0530

    expose restcontroller default message

---
+++ src/main/java/com/boot/das_boot/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.boot.das_boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/") //mapping the path
+	public String home() {
+		return "Das Boot, reporting for duty!";		
+	}
+}
