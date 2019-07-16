38bc0a5e53dc0f04fa1c9393a8b2a401d59fa476
Nikhil Balyan <nbalyan10@gmail.com>
2019-07-16 12:37:53 +0530

    add jpa repository and jpa repository interface

---
+++ src/main/java/com/boot/das_boot/repository/ShipwreckRepository.java
@@ -0,0 +1,9 @@
+package com.boot.das_boot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.boot.das_boot.model.Shipwreck;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long>{
+
+}
