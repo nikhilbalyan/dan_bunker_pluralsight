36ffaa060ecc992d6eff5b273d0298309a8cf051
Nikhil Balyan <nbalyan10@gmail.com>
2019-07-17 17:01:17 +0530

    implement integration test in spring boot

---
+++ src/test/java/com/boot/das_boot/ShipwreckRepositoryIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.boot.das_boot;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.boot.das_boot.model.Shipwreck;
+import com.boot.das_boot.repository.ShipwreckRepository;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(App.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ShipwreckRepositoryIntegrationTest {
+
+	@Autowired
+	private ShipwreckRepository shipwreckRepository;
+	
+	@Test
+	public void testFindAll() {
+		List<Shipwreck> wrecks = shipwreckRepository.findAll();
+		assertThat(wrecks.size(), is(greaterThanOrEqualTo(0)));
+	}
+}
